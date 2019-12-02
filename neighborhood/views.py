a50739652f8fd89a0ccefad7a356707f6879dea1
Titus Ouko <titusouko@gmail.com>
2019-12-02 15:42:07 +0300

    Add view to display hood info

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -4,3 +4,9 @@ from .models import *
 def index(request):
     hoods = Neighborhood.objects.all()
     return render(request, 'main/index.html', {'hoods':hoods})
+
+def hood(request,id):
+    hoods = Neighborhood.objects.get(id=id)
+
+    
+    return render(request, 'main/hood.html', {'hoods':hood})
