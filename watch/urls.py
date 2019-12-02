6c6eede984374a1fb0e88b38bb562a871e979da4
Titus Ouko <titusouko@gmail.com>
2019-12-02 21:27:59 +0300

    Add view function for logout

--- watch/urls.py
+++ watch/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('neighborhood.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.LogoutView.as_view(), {'next':'/'})
    
 ]
