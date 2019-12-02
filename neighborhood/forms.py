4b00688ef364722d51de6c6797cece02dc63add9
Titus Ouko <titusouko@gmail.com>
2019-12-02 17:49:09 +0300

    Add form for editing profile

---
+++ neighborhood/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Profile,Post
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude=['user']
+
+class ProstForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude=['user']
+
