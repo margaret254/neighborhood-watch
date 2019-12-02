67fbaa2a748d408de048b4b505555c1d175287d7
Titus Ouko <titusouko@gmail.com>
2019-12-02 17:50:26 +0300

    Add views for profile pages

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import *
 
 def index(request):
     hoods = Neighborhood.objects.all()
@@ -7,6 +10,25 @@ def index(request):
 
 def hood(request,id):
     hoods = Neighborhood.objects.get(id=id)
+    bus = hoods.business_set.all
+    posts  = hoods.post_set.all
+    return render(request, 'main/hood.html', {'hoods':hoods, 'bus':bus, 'posts':posts})
 
-    
-    return render(request, 'main/hood.html', {'hoods':hood})
+def profile(request):
+      current_user = request.user
+      profile = Profile.objects.filter(user=current_user).first()
+      posts = request.user.post_set.all()
+       
+      return render(request, 'main/profile.html', {"posts": posts, "profile": profile, 'current_user':current_user})
+
+def update_profile(request):
+    user_profile = Profile.objects.get(user=request.user)
+    # form = UpdateProfileForm(instance=request.user)
+    if request.method == "POST":
+        form =  ProfileForm(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+        return render(request,'main/update_profile.html', {'form':form})
