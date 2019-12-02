f9ac5fbb0ae09811637bca26cc50c5cfde34d942
Titus Ouko <titusouko@gmail.com>
2019-12-02 12:31:39 +0300

    Add logout url

--- watch/urls.py
+++ watch/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('neighborhood.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('logout/', views.logout,{"next_page":'/'}),
     # path('logout/', views.logout, {"next_page": '/'}),
 ]
