30a97de70b3ae12b9a7ea1521601a53c236cd02e
Titus Ouko <titusouko@gmail.com>
2019-12-02 14:46:39 +0300

    Add heroku configurations

--- watch/settings.py
+++ watch/settings.py
@@ -1,5 +1,7 @@
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +130,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
