4c6cca5376c82c6a2dcf53f6eab650cb626c3e0e
Titus Ouko <titusouko@gmail.com>
2019-12-03 10:04:32 +0300

    Add id in url for specific paths

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import  static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('hood/<int:id>/', views.hood, name='hood'),
-    path('profile/', views.profile, name='profile'),
-    path('update/', views.update_profile, name='update'),
-    path('post/<int:id>/', views.add_post, name='new_post')
+    url(r'^$', views.index, name='index'),
+    url(r'^hood/(?P<id>\d+)/', views.hood, name='hood'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^update/$', views.update_profile, name='update'),
+    url(r'^post/(?P<id>\d+)', views.add_post, name='new_post')
     
 ]
 if settings.DEBUG:
