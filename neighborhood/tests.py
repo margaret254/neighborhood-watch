74edf2eee40f47e06cb1b194ffef1de0e16ce9e4
Titus Ouko <titusouko@gmail.com>
2019-12-03 14:17:51 +0300

    Add test for profile behavior

--- neighborhood/tests.py
+++ neighborhood/tests.py
@@ -9,26 +9,27 @@ class ProfileTestClass(TestCase):
     #Set up method
 
     def setUp(self):
-        self.new_user = User(username='Titus', email='titusouko@gmail.com', password='1234')
-        self.new_user.save()
-        self.new_hood = Neighborhood(name="Lavington", location="Nairobi", occupants="333",health_contact="123", police_contact="444", hood_pic="me.png", admin=self.new_user)
-        self.new_hood.save()
-        self.new_profile = Profile(user=self.new_user,hood=self.new_hood,bio="just testing", email='titusouko@gmail.com',name="Titus",profile_pic="image.jpeg")
+        
+        self.new_profile = Profile(user_id=2,hood_id=3,bio="just testing", email='titusouko@gmail.com',name="Titus",profile_pic="image.jpeg")
     
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.new_profile,Profile))
     
-    # def test_save_method(self):
-    #     self.new_profile.save_profile()
-    #     profile = Profile.objects.all()
-    #     self.assertTrue(len(profile)>0)
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
 
-    # def test_delete_method(self):
-    #     self.new_profile.save_profile()
-    #     self.new_profile.delete_profile()
-    #     profile = Profile.objects.all()
-    #     self.assertTrue(len(profile)==0)   
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0) 
+    
+    def tearDown(self):
+        Profile.objects.all().delete()
+
 class PostTestClass(TestCase):
 
     def setUp(self):
@@ -53,15 +54,26 @@ class PostTestClass(TestCase):
         post = Post.objects.all()
         self.assertTrue(len(post)==0)
 
+    def test_update_post_method(self):
+        self.new_post.save_post()
+        new_title = 'the cold' 
+        update = self.new_post.update_post(self.new_post.id,new_title)
+        self.assertEqual(update,new_title)
+        
+    def test_find_method(self):
+        self.new_post.save_post()
+        post = self.new_post.get_single_post(self.new_post.id)
+        self.assertEquals(post.title,'techs')
+
+    def tearDown(self):
+        Post.objects.all().delete()
+
 class BuninessTestClass(TestCase):
 
     def setUp(self):
         
-        self.new_user = User(username='Titus', email='titusouko@gmail.com', password='1234')
-        self.new_user.save()
-        self.new_hood = Neighborhood(name="Lavington", location="Nairobi", occupants="333",health_contact="123", police_contact="444", hood_pic="me.png", admin=self.new_user)
-        self.new_hood.save()
-        self.new_business=Business(bName="techs",user=self.new_user, hood=self.new_hood, bEmail="titusouko@gmail.com")
+        
+        self.new_business=Business(bName="techs",user_id=4, hood_id=2, bEmail="titusouko@gmail.com")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_business,Business))    
@@ -71,11 +83,27 @@ class BuninessTestClass(TestCase):
         business = Business.objects.all()
         self.assertTrue(len(business)>0)
 
-    # def test_delete_business(self):
-    #     self.new_business.create_business()
-    #     self.new_business.delete_business()
-    #     business = Business.objects.all()
-    #     self.assertTrue(len(business)==0)
+   
+
+    def test_delete_method(self):
+        self.new_business.create_business()
+        self.new_business.delete_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business) is 0)
+
+    def test_update_bussiness_method(self):
+        self.new_business.create_business()
+        new_name = 'wannaa' 
+        update = self.new_business.update_business(self.new_business.id,new_name)
+        self.assertEqual(update,new_name)
+        
+    def test_find_method(self):
+        self.new_business.create_business()
+        bussiness = self.new_business.find_business(self.new_business.id)
+        self.assertEquals(bussiness.bName,'techs')
+
+    def tearDown(self):
+        Business.objects.all().delete()
 
 class NeighborhoodTestClass(TestCase):
 
@@ -99,3 +127,12 @@ class NeighborhoodTestClass(TestCase):
         self.new_hood.delete_neigborhood()
         hood = Neighborhood.objects.all()
         self.assertTrue(len(hood)==0)
+
+    def test_update_neigborhood_method(self):
+        self.new_hood.create_neigborhood()
+        new_name = 'Lavington' 
+        update = self.new_hood.update_neighborhood(self.new_hood.id,new_name)
+        self.assertEqual(update,new_name)
+
+    def tearDown(self):
+        Neighborhood.objects.all().delete() 
